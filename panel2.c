a1cf92b33d66c302d4412ab646b5d80b952ba77d
mdy99 <162884184+mdy99@users.noreply.github.com>
2024-11-18 16:24:58 +0900

    Update panel2.c

--- panel2.c
+++ panel2.c
@@ -9,18 +9,18 @@ int main() {
     cbreak();
     noecho();
 
-    // 패널 시스템 초기화
+    // 윈도우, 패널 배열 생성
     PANEL *panels[NUM];
     WINDOW *windows[NUM];
 
-    // 두 개의 패널을 생성
+    
     for (int i = 0; i < NUM; i++) {
-        windows[i] = newwin(10, 30, 5 + (5 * i), 5 + (5 * i));
-        box(windows[i], 0, 0);
-        panels[i] = new_panel(windows[i]);
+        windows[i] = newwin(10, 30, 5 + (5 * i), 5 + (5 * i)); // 윈도우 생성 후 WINDOW 포인터에 담아주기
+        box(windows[i], 0, 0); // 보기 쉽게 박스 씌우기
+        panels[i] = new_panel(windows[i]); // 윈도우를 패널 위에 올림
     }
-
-    for (int i = 0; i < 12; i++) {
+    // 3개의 패널이 1초 간격으로 최상단으로 올라옴
+    for (int i = 0; i < 12; i++) { 
         top_panel(panels[(i % 3)]);
         update_panels();
         doupdate();
